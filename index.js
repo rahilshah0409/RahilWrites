542952f2b962a5f424f0efc9b25852543f5b4b6e
ras19 <ras19@ic.ac.uk>
2021-01-07 11:35:41 +0000

    Changed first parallax image and reduced size of headline photo on screens

--- index.js
+++ index.js
@@ -8,18 +8,40 @@ $(document).ready(function () {
   $('.scrollspy').scrollSpy();
 });
 
-// Wait for the page to finish loading
+// Vanilla JS
+
+/* ------- For contact form -------- */
 document.addEventListener('DOMContentLoaded', function () {
-    // Attach `change` event listener to checkbox
-    document.getElementById('othercheckbox').onchange = toggleOtherText;
-  }, false);
+  // Attach `change` event listener to checkbox
+  document.getElementById('othercheckbox').onchange = toggleOtherText;
+}, false);
 
-  function toggleOtherText() {
-    document.getElementById('othertextarea').disabled = !document.getElementById('othertextarea').disabled;
+function toggleOtherText() {
+  document.getElementById('othertextarea').disabled = !document.getElementById('othertextarea').disabled;
 
-    if (!document.getElementById('othertextarea').disabled) {
-      document.getElementById('otherlabel').style.color = "#1a237e";
-    } else {
-      document.getElementById('otherlabel').style.color = "grey";
-    }
+  if (!document.getElementById('othertextarea').disabled) {
+    document.getElementById('otherlabel').style.color = "#1a237e";
+  } else {
+    document.getElementById('otherlabel').style.color = "grey";
   }
+}
+
+// Modal for submission pop-up after checking that relevant form questions have been filled in
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.modal');
+//   var instances = M.Modal.init(elems, options);
+// });
+
+document.addEventListener('DOMContentLoaded', enableSubmit(), false);
+
+function enableSubmit() {
+  let contactform = document.forms["contactform"];
+  let fullname = contactform["fullname"];
+  let email = contactform["email"];
+
+  if (fullname.value != "") {
+    document.getElementById('modalbtn').disabled = false;
+  }
+}
+
+enableSubmit();
